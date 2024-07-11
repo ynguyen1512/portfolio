384505de62c103dace89d315dad6304dc24cb5a2
Richard Nguyen <nguyenphuyfgw@gmail.com>
2024-07-11 13:37:13 +0700

    add some variable

--- next.config.mjs
+++ next.config.mjs
@@ -8,7 +8,7 @@ export default withSentryConfig(nextConfig, {
 
 // Suppresses source map uploading logs during build
 silent: true,
-org: "javascript-mastery",
+org: "richard-ovi",
 project: "javascript-nextjs",
 }, {
 // For all available options, see:
--- sentry.edge.config.ts
+++ sentry.edge.config.ts
@@ -6,7 +6,7 @@
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
-  dsn: "https://7b883d075caa19b41fd9b00ae313a1c6@o4506813739368448.ingest.us.sentry.io/4507222371729408",
+  dsn: "https://2302cf8dbadee27b33578d51c92dd2f9@o4507581026992128.ingest.us.sentry.io/4507581740351488",
 
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1,
